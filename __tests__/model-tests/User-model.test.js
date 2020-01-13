8691cda594b0364a461c76664497c2bb4c236004
Zak Hyslin <rapscalan@gmail.com>
2020-01-13 15:32:41 -0800

    async removal

--- __tests__/model-tests/User-model.test.js
+++ __tests__/model-tests/User-model.test.js
@@ -1,7 +1,7 @@
 const User = require('../../lib/models/User');
 
 describe('User model', () => {
-  it('defaults a display name to email user', async() => {
+  it('defaults a display name to email user', () => {
     const user = new User({
       email: 'test@test.com',
       role: 'user'
@@ -12,7 +12,7 @@ describe('User model', () => {
     expect(user.displayName).toEqual('test');
   });
 
-  it('requires an email', async() => {
+  it('requires an email', () => {
     const user = new User({
       password: 'password',
       role: 'user'
@@ -23,7 +23,7 @@ describe('User model', () => {
     expect(errors.email.message).toEqual('Path `email` is required.');
   });
 
-  it('requires a password', async() => {
+  it('requires a password', () => {
     const user = new User({
       email: 'test@test.com',
       role: 'user'
@@ -34,7 +34,7 @@ describe('User model', () => {
     expect(errors.passwordHash.message).toEqual('Path `passwordHash` is required.');
   });
 
-  it('rejects a non "user" or "admin" role', async() => {
+  it('rejects a non "user" or "admin" role', () => {
     const failedUser = new User({
       email: 'test@test.com',
       password: 'password',
@@ -56,5 +56,4 @@ describe('User model', () => {
     expect(user.displayName).toEqual('test@test@@weirdusername');
   })
 
-  
 });
