a00d54434b39e9fe9e6cfe795c844bfced4f693f
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-01-14 14:44:50 -0800

    filter array seed date created and formated, data imported to searchpageApp.js

--- public/components/searchpageapp/SearchPageApp.js
+++ public/components/searchpageapp/SearchPageApp.js
@@ -1,8 +1,11 @@
 import Component from '../Component.js';
 import Header from '../header/Header.js';
 import DropDown from './DropDown.js';
+import  { naturalTherapies, conventionalTreatementTypes, sideEffects, yearPublished, studyDesigns, studyDesignFeatures, populationSizes, adverseEvents, tumorType, stage, outcomes, outcomeTypes, interactions, naturalTerapyTypes, conventionalTreatmentTypes } from './dropDownSeedData.js';
 
+console.log(naturalTherapies, conventionalTreatementTypes, sideEffects, yearPublished, studyDesigns, studyDesignFeatures, populationSizes, adverseEvents, tumorType, stage, outcomes, outcomeTypes, interactions, naturalTerapyTypes, conventionalTreatmentTypes);
 
+///// submit button, clear all, [search box]
 
 class Filter extends Component {
   onRender(dom) {
@@ -10,26 +13,29 @@ class Filter extends Component {
     dom.prepend(header.renderDOM());
     const filterdiv = dom.querySelector('#filter');
     const filterArrays = [
-      ['Year Published', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009', '2008', '2007', '2006', '2005', '2004', '2003', '2002', '2001', '2000', '1999', '1998', '1997', '1996', '1995', '1994', '1993', '1992', '1991', '1989', '1988', '1987', '1984', '1981', '1974', '1969']
+    
+      
+
     ];
     filterArrays.forEach(array => {
       const dropDown = new DropDown(array); 
       filterdiv.appendChild(dropDown.renderDOM());
     });
-
   }
+  
+
 
   renderHTML(){
     return /*html*/`
      <div> 
         <div id='filter'>
-
+        
         </div>
     </div>
     `;
-      
+    
   }
-
+  
 }
 
 export default Filter;
