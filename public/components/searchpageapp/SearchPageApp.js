5f418e6cb3534c80f314f665fe6224c4a93f3098
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-01-16 15:01:35 -0800

    'loading styled'

--- public/components/searchpageapp/SearchPageApp.js
+++ public/components/searchpageapp/SearchPageApp.js
@@ -2,6 +2,7 @@ import Component from '../Component.js';
 import Header from '../header/Header.js';
 import DropDown from './DropDown.js';
 import  { naturalTherapies, conventionalTreatementTypes, sideEffects, yearPublished, studyDesigns, studyDesignFeatures, populationSizes, adverseEvents, tumorType, stage, outcomes, outcomeTypes, interactions, naturalTerapyTypes, conventionalTreatmentTypes } from './dropDownSeedData.js';
+import Loading from '../loading/loading.js';
 
 ///// submit button, clear all, [search box]
 
@@ -25,9 +26,13 @@ class Filter extends Component {
       dom.appendChild(dropDown.renderDOM());
     });
 
+    const loading = new Loading({ loading: true });
+    
     const searchButton = document.createElement('button');
     searchButton.textContent = 'Search';
     searchButton.addEventListener('click', async() => {
+      dom.appendChild(loading.renderDOM());
+      
       const searchTextInput = searchInput.value;
       const searchObject = { 
         searchTextInput, 
@@ -40,8 +45,12 @@ class Filter extends Component {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(searchObject)
-      });
+        
+      })
+        .then(loading.update({ loading: false }));
+      console.log('loader here and gone');
     });
+
     dom.appendChild(searchButton);
 
     // eslint-disable-next-line no-console
