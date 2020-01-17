074046b40d0526138d6398b828aad092898f550a
drmeloy <drmeloy@gmail.com>
2020-01-16 20:01:34 -0700

    more styling, added counters

--- public/components/searchpageapp/ResultsSection.js
+++ public/components/searchpageapp/ResultsSection.js
@@ -6,8 +6,13 @@ class ResultsSection extends Component {
     const summaries = this.props;
     const resultsHeader = document.createElement('h1');
     resultsHeader.id = 'results-header';
-    resultsHeader.textContent = 'Search Results';
+    resultsHeader.textContent = 'Summarized Search Results';
     dom.prepend(resultsHeader);
+
+    const caseReportResultsSection = dom.querySelector('#case-report-results');
+    const clinicalTrialResultsSection = dom.querySelector('#clinical-trial-results');
+    const observationalResultsSection = dom.querySelector('#observational-results');
+    const systemicReviewResultsSection = dom.querySelector('#systematic-review-results');
     
     if(!summaries[0]){
       const noResults = document.createElement('div');
@@ -18,14 +23,64 @@ class ResultsSection extends Component {
 
     summaries.forEach(summary => {
       const resultItem = new ResultItem(summary);
-      dom.appendChild(resultItem.renderDOM());
+      const studyType = summary.studyType.toLowerCase();
+      if(studyType === 'case report/series'){
+        caseReportResultsSection.appendChild(resultItem.renderDOM());
+      } 
+      else if(studyType === 'clinical trial'){
+        clinicalTrialResultsSection.appendChild(resultItem.renderDOM());
+      }
+      else if(studyType === 'observational results'){
+        observationalResultsSection.appendChild(resultItem.renderDOM());
+      }
+      else if(studyType === 'systematic review and/or meta-analysis'){
+        systemicReviewResultsSection.appendChild(resultItem.renderDOM());
+      }
+      else {
+        dom.appendChild(resultItem.renderDOM());
+      }
     });
 
   }
 
   renderHTML(){
+    const summaries = this.props;
+    let caseReportCount = 0;
+    let clinicalTrialCount = 0;
+    let observationalCount = 0;
+    let systematicCount = 0;
+
+    summaries.forEach(summary => {
+      const studyType = summary.studyType.toLowerCase();
+      if(studyType === 'case report/series'){
+        caseReportCount++;
+      } 
+      else if(studyType === 'clinical trial'){
+        clinicalTrialCount++;
+      }
+      else if(studyType === 'observational results'){
+        observationalCount++;
+      }
+      else if(studyType === 'systematic review and/or meta-analysis'){
+        systematicCount++;
+      }
+    });
+
     return /*html*/ `
-      <section id="results"></section>
+      <section id="results">
+        <details id='case-report-results'>
+          <summary class='results-list'>Case Report/Series Results<span class='counter'>Total results: ${caseReportCount}</span></summary>
+        </details>
+        <details id='clinical-trial-results'>
+          <summary class='results-list'>Clinical Trial Results<span class='counter'>Total results: ${clinicalTrialCount}</span></summary>
+        </details>
+        <details id='observational-results'>
+          <summary class='results-list'>Observational Results<span class='counter'>Total results: ${observationalCount}</span></summary>
+        </details>
+        <details id='systematic-review-results'>
+          <summary class='results-list'>Systematic Review and/or Meta-analysis Results<span class='counter'>Total results: ${systematicCount}</span></summary>
+        </details>
+      </section>
     `;
   }
 }
