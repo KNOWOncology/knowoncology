e9d707a8edf04452970d814c50db31a4f018dfa1
drmartell <drmartell@gmail.com>
2020-01-19 21:39:39 -0800

    additional comments removed

--- server.js
+++ server.js
@@ -14,5 +14,4 @@ const PORT = process.env.PORT || 7890;
 if(typeof(PhusionPassenger) !== 'undefined') app.listen('passenger');
 else app.listen(PORT, () => console.log('server running on PORT', PORT)); // eslint-disable-line no-console
 
-// Check for new summaries in Airtable at 1 AM
 new Cron('0 1 * * *', getNewSummaries, null, true, 'America/Los_Angeles');
